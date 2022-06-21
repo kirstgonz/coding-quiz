0c84ba4a2844706132b383015869df985c2de398
kirstgonz <104031234+kirstgonz@users.noreply.github.com>
2022-06-20 23:19:14 -0500

    Added timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,37 @@
-
-// addEventListener("click", startButton)
-
-// var startButton = function(event) {
-//     document.getElementById("button").addEventListener.("click," )
-//     console.log("clicked!");
-// }
-
-//startButton();
 var startButton = document.querySelector("#start-btn");
 
+// Clicking start to start the test. It makes the text disappear and calls the workingTimer function to start the 1 minute timer
 startButton.addEventListener("click", function (event) {
     document.getElementById("intro").style.display = "none"
+    workingTimer();
 });
 
+// Making the timer hidden at 01:00
+var timerEl = document.getElementById("timer")
+    timerEl.innerHTML="<p>01:00</p>";
+    timerEl.style.visibility="hidden";
+
+var seconds = 59;
+
+// Timer set to 1 minute
+function workingTimer() {
+timerEl.style.visibility = "visible";
+var timer = setInterval(function() {
+    seconds--;
+      if (seconds >= 10) {
+        timerEl.innerHTML="<p>00:"+seconds+"</p>";
+      }
+      if (seconds < 10) {
+        timerEl.innerHTML="<p>00:0"+seconds;
+      }
+      if (seconds <= 0) {
+        clearInterval(timer);
+        //CALL END GAME FUNCTION?
+      }  
+}, 1000);
+}
 
 
-var seconds = 5;
-var timer = "";
 
 
 
